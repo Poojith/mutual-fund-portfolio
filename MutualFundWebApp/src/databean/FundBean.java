e162e1f8550038ae0af1746ab0c31b61ddf0b810
Poojith <poojithjain@live.com>
2017-01-21 15:00:23 -0500

    Format databeans.

--- MutualFundWebApp/src/databean/FundBean.java
+++ MutualFundWebApp/src/databean/FundBean.java
@@ -1,5 +1,4 @@
 
-
 package databean;
 
 import org.genericdao.MaxSize;
@@ -7,17 +6,33 @@ import org.genericdao.PrimaryKey;
 
 @PrimaryKey("FundId")
 public class FundBean {
-	private int    fundId;
+	private int fundId;
 	private String name;
 	private String symbol;
-	
-	public int    getFundId()                { return fundId;           }
-    public String getName()              { return name;         }
-    public String    getSymbol()          { return symbol;     }
-    
-	public void   setFundId(int i)                { fundId = i;           }
+
+	public int getFundId() {
+		return fundId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setFundId(int i) {
+		fundId = i;
+	}
+
 	@MaxSize(50)
-	public void setName(String s)              { name = s;         }
+	public void setName(String s) {
+		name = s;
+	}
+
 	@MaxSize(50)
-	public void    setSymbol(String s)          { symbol = s;     }
+	public void setSymbol(String s) {
+		symbol = s;
+	}
 }
--- MutualFundWebApp/src/databean/PositionBean.java
+++ MutualFundWebApp/src/databean/PositionBean.java
@@ -6,15 +6,31 @@ import org.genericdao.PrimaryKey;
 
 @PrimaryKey("customerId, fundId")
 public class PositionBean {
-	private int    customerId;
-	private int    fundId;
+	private int customerId;
+	private int fundId;
 	private int shares;
-	
-    public int getCustomerId()              { return customerId;         }
-    public int    getFundId()          { return fundId;     }
-    public int getShares()          { return shares;     }
-    
-	public void   setCustomerId(int i)      { customerId = i;            }
-	public void   setFundId(int i)     { fundId = i;        }
-	public void   setShares(int i)  { shares = i;        }
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public int getFundId() {
+		return fundId;
+	}
+
+	public int getShares() {
+		return shares;
+	}
+
+	public void setCustomerId(int i) {
+		customerId = i;
+	}
+
+	public void setFundId(int i) {
+		fundId = i;
+	}
+
+	public void setShares(int i) {
+		shares = i;
+	}
 }
