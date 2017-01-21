7a23efbfecef9882e1a8b0165c4a6fa4ce75dacd
Poojith <poojithjain@live.com>
2017-01-21 14:45:02 -0500

    Add Action and Controller classes

--- MutualFundWebApp/src/controller/Controller.java
+++ MutualFundWebApp/src/controller/Controller.java
@@ -1,5 +1,107 @@
 package controller;
 
-public class Controller {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import databean.CustomerBean;
+//import model.Model;
+
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Controller() {
+        super();
+    }
+    
+    public void init() {
+    	/*
+    	Model model = new Model(getServletConfig());
+    	
+    	Action.add(new BuyFundAction(model));
+    	Action.add(new ChangePasswordAction(model));
+    	Action.add(new CreateCustomerAccountAction(model));
+    	Action.add(new CreateEmployeeAccountAction(model));
+    	Action.add(new CreateFundAction(model));
+    	Action.add(new DepositCheckAction(model));
+    	Action.add(new LoginAction(model));
+    	Action.add(new LogoutAction(model));
+    	Action.add(new RequestCheckAction(model));
+    	Action.add(new ResearchFundAction(model));
+    	Action.add(new ResetCustomerPasswordAction(model));
+    	Action.add(new SellFundAction(model));
+    	Action.add(new TransactionHistoryAction(model));
+    	Action.add(new TransitionDayAction(model));
+    	Action.add(new ViewAccountAction(model));
+    	Action.add(new ViewCustomerAccountAction(model));
+    	Action.add(new ViewCustTransactionHistAction(model));	
+    	
+    	*/
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String nextPage = performAction(request);
+		sendToNextPage(nextPage, request, response);
+	}
+
+	private String performAction(HttpServletRequest request) {
+		HttpSession session = request.getSession(true);
+		String servletPath = request.getServletPath();
+		String action = getActionName(servletPath);
+		CustomerBean customer = (CustomerBean) session.getAttribute("user");
+		//need to similarly check for employee
+		
+		if(action.equals("login.do")) {
+			return Action.perform(action, request); 
+		}
+
+		
+		if(customer == null) {
+			return Action.perform("login.do", request);
+		}
+			
+		/*
+		 * Logic for other scenarios goes in here
+		 */
+		
+		return Action.perform(action, request);		
+	}
+
+	private String getActionName(String servletPath) {
+		int index = servletPath.lastIndexOf('/');
+		return servletPath.substring(index + 1);
+	}
+	
+	private void sendToNextPage(String nextPage, HttpServletRequest request, HttpServletResponse response) 
+			throws IOException, ServletException {
+		
+		if(nextPage == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND, request.getServletPath());
+		}
+		
+		if(nextPage.endsWith(".do")) {
+			response.sendRedirect(nextPage);
+			return;
+		}
+		
+		if(nextPage.endsWith(".jsp")) {
+			RequestDispatcher dispathcer = request.getRequestDispatcher("WEB-INF/" + nextPage);
+			dispathcer.forward(request, response);
+			return;
+		}
+		
+		throw new ServletException( 
+				Controller.class.getName() + ".sendToNextPage(\"" + nextPage + "\"): invalid extension.");
+	}
 }
