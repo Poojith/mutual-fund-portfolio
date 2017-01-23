cc322469bb55cf5bb3d274c38f5c114d06526804
Wei Lu <wlu2@andrew.cmu.edu>
2017-01-23 10:33:18 -0500

    Update Buyfund and sellfund form beans

--- MutualFundWebApp/src/formbeans/BuyFundForm.java
+++ MutualFundWebApp/src/formbeans/BuyFundForm.java
@@ -2,42 +2,38 @@ package formbeans;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.mybeans.form.FormBean;
 
-import javax.servlet.http.HttpServletRequest;
-
-public class BuyFundForm {
+public class BuyFundForm extends FormBean {
 	private String fundName;
 	private String buyAmount;
-	private String buyFundButton;
-	
-	public BuyFundForm(HttpServletRequest request) {
-		fundName = request.getParameter("fund");
-		String amount = request.getParameter("buyAmount");
-		if(amount != null) {
-			buyAmount = amount.trim();
-		}
-		buyFundButton = request.getParameter("action");	
-	}
-	
+	private String buyFund;
+
 	public String getFundName() {
 		return fundName;
 	}
-
 	public String getBuyAmount() {
 		return buyAmount;
 	}
 	
-	/*
-	 * getValidationErrors() must be called prior to this method to ensure no exception is thrown.
-	 */
-	public double getBuyAmountAsDouble() {
+	public void setFundName(String fundName) {
+		this.fundName = fundName.trim();
+	}
+
+	public void setBuyAmount(String buyAmount) {
+		this.buyAmount = buyAmount.trim();
+	}
+
+	public double getAmountDouble() {
 		return Double.parseDouble(buyAmount);
 	}
 	
-	public boolean isPresent() {
-		return buyFundButton != null;
+	public String getBuyFund() {
+		return buyFund;
 	}
-	
+	public void setBuyFund(String buyFund) {
+		this.buyFund = buyFund;
+	}
 	public List<String> getValidationErrors() {
 		List<String> errors = new ArrayList<String>();
 		
@@ -48,17 +44,11 @@ public class BuyFundForm {
 		if(buyAmount == null || buyAmount.length() == 0) {
 			errors.add("Please specify the amount in order to buy the fund");
 		}
-		
-		if(buyFundButton == null) {
-			errors.add("Button is required");
+		if(buyFund == null) {
+			errors.add("Buy Fund button is required");
 		}
-		
 		if(errors.size() > 0) {
 			return errors;
-		}
-		
-		if(!buyFundButton.equals("Buy fund")) {
-			errors.add("Invalid button.");
 		}
 		
 		try {
