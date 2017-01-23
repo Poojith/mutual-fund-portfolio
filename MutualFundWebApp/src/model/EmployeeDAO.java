29112880c418da67c52189af475d712c040d907a
Shu Wang <shuwang@andrew.cmu.edu>
2017-01-22 19:40:48 -0500

    Some updated DAO

--- MutualFundWebApp/src/model/EmployeeDAO.java
+++ MutualFundWebApp/src/model/EmployeeDAO.java
@@ -9,8 +9,53 @@ import org.genericdao.Transaction;
 
 import databean.EmployeeBean;
 
-public class EmployeeDAO extends GenericDAO{
+public class EmployeeDAO extends GenericDAO<EmployeeBean>{
 	public EmployeeDAO(ConnectionPool cp, String tableName) throws DAOException {
         super(EmployeeBean.class, tableName, cp);
+    }
+	public void create (EmployeeBean bean)throws RollbackException {
+    	try {
+    		Transaction.begin();
+    		EmployeeBean employees[] = match(MatchArg.equals("username", bean.getUsername()));
+    		if (employees != null && employees.length > 0) {
+        		throw new RollbackException ("Username already exists");
+        	}
+    		super.create(bean);
+    		Transaction.commit();
+    	} finally {
+    		if (Transaction.isActive()) {
+    			Transaction.rollback();
+    		}
+    	}
+    }
+	
+	public EmployeeBean read (String username) throws RollbackException{
+    	EmployeeBean employees[] = match(MatchArg.equals("username", username));
+    	if (employees ==null || employees.length == 0) {
+    		throw new RollbackException ("No customer for this username");
+    	} else if (employees.length > 1) {
+    		throw new RollbackException ("Multiple customers for this username");
+    	}
+    	return employees[0];
+    }
+	
+	public EmployeeBean changePassword(String username, String password) throws RollbackException {
+        try {
+            Transaction.begin();
+            EmployeeBean[] employees = (EmployeeBean[])match(MatchArg.equals("username", username));
+
+            if (employees == null || employees.length == 0) {
+                throw new RollbackException("Employee " + username + " no longer exists");
+            }
+
+            employees[0].setPassword(password);
+
+            update(employees[0]);
+            Transaction.commit();
+            
+            return employees[0];
+        } finally {
+            if (Transaction.isActive()) Transaction.rollback();
+        }
     }
 }
--- MutualFundWebApp/src/model/FundDAO.java
+++ MutualFundWebApp/src/model/FundDAO.java
@@ -7,10 +7,56 @@ import org.genericdao.MatchArg;
 import org.genericdao.RollbackException;
 import org.genericdao.Transaction;
 
+import databean.EmployeeBean;
 import databean.FundBean;
+import databean.TransactionBean;
 
-public class FundDAO extends GenericDAO{
+public class FundDAO extends GenericDAO<FundBean>{
 	public FundDAO(ConnectionPool cp, String tableName) throws DAOException {
         super(FundBean.class, tableName, cp);
     }
+	
+	public void create (FundBean bean)throws RollbackException {
+    	try {
+    		Transaction.begin();
+    		FundBean funds[] = match(MatchArg.equals("name", bean.getName()));
+    		if (funds != null && funds.length > 0) {
+        		throw new RollbackException ("Fund name already exists");
+        	}
+    		FundBean funds2[] = match(MatchArg.equals("symbol", bean.getSymbol()));
+    		if (funds2 != null && funds2.length > 0) {
+    			throw new RollbackException ("Fund symbol already exists");
+    		}
+    		super.create(bean);
+    		Transaction.commit();
+    	} finally {
+    		if (Transaction.isActive()) {
+    			Transaction.rollback();
+    		}
+    	}
+    }
+	
+	public void research (String symbol) throws RollbackException {
+		FundBean funds[] = match(MatchArg.equals("symbol", symbol));
+    	if (funds ==null || funds.length == 0) {
+    		throw new RollbackException ("No fund for this name");
+    	} 
+    	
+    	return employees[0];
+	}
+//	public void deposit (String name, int amt, TransactionBean bean) throws RollbackException {
+//		try {
+//			Transaction.begin();
+//			FundBean funds[] = match(MatchArg.equals("name", name));
+//			if (funds == null || funds.length == 0) {
+//				throw new RollbackException("Fund " + name + " does not exists");
+//			}
+//			
+//			
+//		} finally {
+//			if (Transaction.isActive()) {
+//				Transaction.rollback();
+//			}
+//		}
+//	}
 }
