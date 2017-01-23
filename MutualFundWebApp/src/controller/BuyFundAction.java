e2f279b2f35b9679ff728f4b88aa95c3ad8252b7
Wei Lu <wlu2@andrew.cmu.edu>
2017-01-23 13:19:04 -0500

    Update Buyfund, Sellfund, pwdchange, requestcheck form beans

--- MutualFundWebApp/src/controller/BuyFundAction.java
+++ MutualFundWebApp/src/controller/BuyFundAction.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class BuyFundAction {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.mybeans.form.FormBeanException;
+import org.mybeans.form.FormBeanFactory;
+
+import daos.MyDAOException;
+import formbeans.BuyFundForm;
+import model.FundDAO;
+import model.Model;
+import model.TransactionDAO;
+
+public class BuyFundAction extends Action {
+	
+	private FormBeanFactory<BuyFundForm> formBeanFactory = FormBeanFactory
+            .getInstance(BuyFundForm.class);
+	private FundDAO fundDAO;
+	private TransactionDAO transactionDAO;
+	
+	public BuyFundAction(Model model) {
+		fundDAO = model.getFundDAO();
+		transactionDAO = model.getTransactionDAO();
+	}
+	
+	public String getName() {
+		return "buyfund.do";
+	}
+
+	public String perform(HttpServletRequest request) {
+		   List<String> errors = new ArrayList<String>();
+	        request.setAttribute("errors", errors);
+	      try {
+	    	  
+	      } catch (Exception e) {
+	            errors.add(e.getMessage());
+	            return "buyfund.jsp";
+	        } catch (FormBeanException e) {
+	            errors.add(e.getMessage());
+	            return "buyfund.jsp";
+	        
+		
+		return null;
+	}
 
 }
--- MutualFundWebApp/src/formbeans/BuyFundForm.java
+++ MutualFundWebApp/src/formbeans/BuyFundForm.java
@@ -2,76 +2,62 @@ package formbeans;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.mybeans.form.FormBean;
 
-import javax.servlet.http.HttpServletRequest;
-
-public class BuyFundForm {
+public class BuyFundForm extends FormBean {
 	private String fundName;
 	private String buyAmount;
-	private String buyFundButton;
-
-	public BuyFundForm(HttpServletRequest request) {
-		fundName = request.getParameter("fund");
-		String amount = request.getParameter("buyAmount");
-		if (amount != null) {
-			buyAmount = amount.trim();
-		}
-		buyFundButton = request.getParameter("action");
-	}
+	private String buyFund;
 
 	public String getFundName() {
 		return fundName;
 	}
-
 	public String getBuyAmount() {
 		return buyAmount;
 	}
 	
-	public String getBuyFundButton() {
-		return buyFundButton;
+	public void setFundName(String fundName) {
+		this.fundName = fundName.trim();
 	}
 
-	/*
-	 * getValidationErrors() must be called prior to this method to ensure no
-	 * exception is thrown.
-	 */
-	public double getBuyAmountAsDouble() {
+	public void setBuyAmount(String buyAmount) {
+		this.buyAmount = buyAmount.trim();
+	}
+
+	public double getAmountDouble() {
 		return Double.parseDouble(buyAmount);
 	}
-
-	public boolean isPresent() {
-		return buyFundButton != null;
+	
+	public String getBuyFund() {
+		return buyFund;
 	}
-
+	public void setBuyFund(String buyFund) {
+		this.buyFund = buyFund;
+	}
 	public List<String> getValidationErrors() {
 		List<String> errors = new ArrayList<String>();
-
-		if (fundName == null || fundName.length() == 0) {
+		
+		if(fundName == null || fundName.length() == 0) {
 			errors.add("Fund name is required.");
 		}
-
-		if (buyAmount == null || buyAmount.length() == 0) {
+		
+		if(buyAmount == null || buyAmount.length() == 0) {
 			errors.add("Please specify the amount in order to buy the fund");
 		}
-
-		if (buyFundButton == null) {
-			errors.add("Button is required");
+		if(buyFund == null) {
+			errors.add("Buy Fund button is required");
 		}
-
-		if (errors.size() > 0) {
+		if(errors.size() > 0) {
 			return errors;
 		}
-
-		if (!buyFundButton.equals("Buy fund")) {
-			errors.add("Invalid button.");
-		}
-
+		
 		try {
 			Double.parseDouble(buyAmount);
-		} catch (NumberFormatException e) {
+		}
+		catch(NumberFormatException e) {
 			errors.add("Error in conversion of amount. Please specify the amount in the right format.");
 		}
-
+		
 		return errors;
 	}
 }
