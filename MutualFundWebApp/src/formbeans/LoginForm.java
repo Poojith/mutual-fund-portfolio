fc1c7495c92e45981790c8b2752dd00aee51b52f
Wei Lu <wlu2@andrew.cmu.edu>
2017-01-23 10:52:47 -0500

    Update Login, changepassword and requestcheck Form Beans

--- MutualFundWebApp/src/formbeans/LoginForm.java
+++ MutualFundWebApp/src/formbeans/LoginForm.java
@@ -1,5 +1,39 @@
 package formbeans;
 
-public class LoginForm {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.mybeans.form.FormBean;
+
+public class LoginForm extends FormBean {
+	private String userName;
+	private String passWord;
+	private String CusLogin;
+	private String EmpLogin;
+	
+	public String getUserName()  { return userName; }
+	public String getPassWord()  { return passWord; }
+	public String getCusLogin()  { return CusLogin; }
+	public String getEmpLogin()  { return EmpLogin; }
+	
+	public void setUserName(String s) { userName = trimAndConvert(s,"<>\"");  }
+	public void setPassWord(String s) {	passWord = s.trim();                  }
+	public void setCusLogin(String cusLogin) {CusLogin = cusLogin;            }
+	public void setEmpLogin(String empLogin) {EmpLogin = empLogin;            }
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+
+		if (userName == null || userName.length() == 0) {
+			errors.add("User Name is required");
+		}
+		
+		if (passWord == null || passWord.length() == 0) {
+			errors.add("Password is required");
+		}
+		if (CusLogin == null && EmpLogin == null) {
+			errors.add("Login button is required");
+		}
+		
+		return errors;
+	}
 }
--- MutualFundWebApp/src/formbeans/PwdchangeForm.java
+++ MutualFundWebApp/src/formbeans/PwdchangeForm.java
@@ -1,5 +1,39 @@
 package formbeans;
 
-public class PwdchangeForm {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.mybeans.form.FormBean;
+
+public class PwdchangeForm extends FormBean {
+	private String confirmPassword;
+	private String newPassword;
+	
+	public String getConfirmPassword() { return confirmPassword; }
+	public String getNewPassword()     { return newPassword;     }
+	
+	public void setConfirmPassword(String s) { confirmPassword = s.trim(); }
+	public void setNewPassword(String s)     { newPassword     = s.trim(); }
+
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+
+		if (newPassword == null || newPassword.length() == 0) {
+			errors.add("New Password is required");
+		}
+		
+		if (confirmPassword == null || confirmPassword.length() == 0) {
+			errors.add("Confirm Pwd is required");
+		}
+		
+		if (errors.size() > 0) {
+			return errors;
+		}
+		
+		if (!newPassword.equals(confirmPassword)) {
+			errors.add("Passwords do not match");
+		}
+
+		return errors;
+	}
 }
