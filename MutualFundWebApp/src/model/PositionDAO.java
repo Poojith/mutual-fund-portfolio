6b4bbba9f3184ce7b43696116d4d62a828a58a6e
Ajinkya Nimbalkar <animbalk@andrew.cmu.edu>
2017-01-23 09:08:05 -0500

    customerDAO, positionDAO, transactionDAO ready

--- MutualFundWebApp/src/model/PositionDAO.java
+++ MutualFundWebApp/src/model/PositionDAO.java
@@ -1,5 +1,7 @@
 package model;
 
+
+
 import org.genericdao.ConnectionPool;
 import org.genericdao.DAOException;
 import org.genericdao.GenericDAO;
@@ -9,8 +11,76 @@ import org.genericdao.Transaction;
 
 import databean.PositionBean;
 
-public class PositionDAO extends GenericDAO{
+public class PositionDAO extends GenericDAO<PositionBean>{
 	public PositionDAO(ConnectionPool cp, String tableName) throws DAOException {
         super(PositionBean.class, tableName, cp);
     }
+	
+	 public void createPosition(PositionBean position) throws RollbackException{
+	        if (position == null) {
+	            return;
+	        }
+	        try {
+	            Transaction.begin();
+	            super.create(position);
+	            Transaction.commit();
+	        } finally {
+	            if (Transaction.isActive()){Transaction.rollback();}
+	        }
+	    }
+	    public void updatePosition(PositionBean position) throws RollbackException{
+	        if (position == null) {
+	            return;
+	        }
+	        try {
+	            Transaction.begin();
+	            super.update(position);
+	            Transaction.commit();
+	        } finally {
+	        	if (Transaction.isActive()){Transaction.rollback();}
+	        }
+	    }
+	    public PositionBean getPosition(int customerId, int fundId) throws RollbackException {
+	        	PositionBean position = super.read(customerId, fundId);
+		        return position;
+	    }
+	    
+	    public PositionBean[] getPositionsByCustomerId(int customerId)  throws RollbackException{	        	
+	    	PositionBean[] beans = match((MatchArg.equals("customerId", customerId)));
+	        if (beans == null || beans.length==0) {
+	        	throw new RollbackException("No poistions for this customerId");
+	        }
+	        return beans;
+	    }
+	    
+	    public PositionBean[] getPositionsByFundId(int fundId)  throws RollbackException{	        	
+	    	PositionBean[] beans = match((MatchArg.equals("fundId", fundId)));
+	        if (beans == null || beans.length==0) {
+	        	throw new RollbackException("No poistions for this fundId");
+	        }
+	        return beans;
+	    }
+	    
+	    
+	    public void deletePosition(PositionBean position) throws RollbackException{
+	        if (position == null) {
+	            return;
+	        }
+	       
+	        try {
+	            Transaction.begin();
+	            super.delete(position);
+	            Transaction.commit();
+	        }  finally {
+	        	if (Transaction.isActive()){Transaction.rollback();}
+	        }
+	    }
+	    public void deletePositionsByCustomerId(int customerId) {
+	        
+	    }
+	    public void deletePositionsByFundId(int fundId) {
+	        
+	    }
+	
+
 }
--- MutualFundWebApp/src/model/TransactionDAO.java
+++ MutualFundWebApp/src/model/TransactionDAO.java
@@ -9,8 +9,66 @@ import org.genericdao.Transaction;
 
 import databean.TransactionBean;
 
-public class TransactionDAO extends GenericDAO{
+public class TransactionDAO extends GenericDAO<TransactionBean>{
 	public TransactionDAO(ConnectionPool cp, String tableName) throws DAOException {
         super(TransactionBean.class, tableName, cp);
+    }
+	
+	public void createTransaction(TransactionBean transaction) throws RollbackException{
+        if (transaction == null) {
+            return;
+        }
+        try {
+            Transaction.begin();
+            super.create(transaction);
+            Transaction.commit();
+        }  finally {
+            if (Transaction.isActive()) { Transaction.rollback(); };
+        }
+    }
+    public void updateTransaction(TransactionBean transaction) throws RollbackException {
+        if (transaction == null) {
+            return;
+        }
+        try {
+            Transaction.begin();
+            super.update(transaction);
+            Transaction.commit();
+        }  finally {
+        	if (Transaction.isActive()) { Transaction.rollback(); };
+        }
+    }
+    public void deleteTransaction(TransactionBean transaction) throws RollbackException{
+        if (transaction == null) {
+            return;
+        }
+        try {
+            Transaction.begin();
+            super.delete(transaction);
+            Transaction.commit();
+        }  finally {
+        	if (Transaction.isActive()) { Transaction.rollback(); };
+        }
+    }
+    public TransactionBean[] findTransactionsByNullExecuteDate() throws RollbackException{
+        TransactionBean[] beans = match(MatchArg.equals("executeDate", null));
+        return beans;
+    }
+    
+    public TransactionBean[] findTransactionsByCustomerId (int customerId) throws RollbackException{
+        TransactionBean[] beans = match(MatchArg.equals("customerId", customerId));
+        return beans;
+    }
+    
+    public TransactionBean[] findTransactionsByFundId(int fundId) throws RollbackException{
+    	TransactionBean[] beans = match(MatchArg.equals("fundId", fundId));
+        return beans;
+    }
+    
+    public void deleteTransactionsByCustomerId(int customerId) {
+        
+    }
+    public void deleteTransactionsByFundId(int fundId) {
+        
     }
 }
