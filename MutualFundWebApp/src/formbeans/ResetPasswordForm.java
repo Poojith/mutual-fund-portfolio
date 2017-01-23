b05219d0b319b94d47c802d608100e57dfb3b6a1
Poojith <poojithjain@live.com>
2017-01-23 15:23:54 -0500

    Update form beans for ViewCustomerAccount and ViewCustomerTransaction.

--- MutualFundWebApp/src/formbeans/ResetPasswordForm.java
+++ MutualFundWebApp/src/formbeans/ResetPasswordForm.java
@@ -10,17 +10,17 @@ public class ResetPasswordForm {
 	private String newPassword;
 	private String confirmPassword;
 	private String resetPasswordButton;
-	
+
 	public ResetPasswordForm(HttpServletRequest request) {
 		String name = request.getParameter("userName");
-		if(name != null) {
+		if (name != null) {
 			userName = name.trim();
 		}
 		newPassword = request.getParameter("password");
 		confirmPassword = request.getParameter("confirmPassword");
 		resetPasswordButton = request.getParameter("action");
 	}
-	
+
 	public String getUserName() {
 		return userName;
 	}
@@ -36,32 +36,32 @@ public class ResetPasswordForm {
 	public String getResetPasswordButton() {
 		return resetPasswordButton;
 	}
-	
+
 	public boolean isPresent() {
 		return resetPasswordButton != null;
 	}
-	
+
 	public List<String> getValidationErrors() {
 		List<String> errors = new ArrayList<String>();
 
 		if (userName == null || userName.length() == 0)
 			errors.add("User name is required.");
-		
+
 		if (newPassword == null || newPassword.length() == 0)
 			errors.add("Password is required.");
 
 		if (confirmPassword == null || confirmPassword.length() == 0)
 			errors.add("Please confirm your password.");
-		
+
 		if (resetPasswordButton == null || !resetPasswordButton.equals("Reset password"))
 			errors.add("Invalid button.");
-		
+
 		if (errors.size() > 0)
 			return errors;
-		
+
 		if (!confirmPassword.equals(newPassword))
 			errors.add("Passwords do not match. Please try again.");
-		
+
 		return errors;
 	}
 }
