cbb2c23bdb2a6972e79e34611d9c59d7775a2a61
Poojith <poojithjain@live.com>
2017-01-23 22:32:33 -0500

    Add CreateFundAction for creating new funds.

--- MutualFundWebApp/src/controller/CreateFundAction.java
+++ MutualFundWebApp/src/controller/CreateFundAction.java
@@ -1,5 +1,65 @@
 package controller;
 
-public class CreateFundAction {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.genericdao.RollbackException;
+
+import databean.FundBean;
+import formbeans.CreateFundForm;
+import model.FundDAO;
+import model.Model;
+
+public class CreateFundAction extends Action {
+	private FundDAO fundDAO;
+
+	public CreateFundAction(Model model) {
+		fundDAO = model.getFundDAO();
+	}
+
+	@Override
+	public String getName() {
+		return "create-fund.do";
+	}
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		List<String> errors = new ArrayList<String>();
+		request.setAttribute("errors", errors);
+
+		try {
+			CreateFundForm form = new CreateFundForm(request);
+			if (!form.isPresent()) {
+				return "employee-create-fund.jsp";
+			}
+
+			errors.addAll(form.getValidationErrors());
+			if (errors.size() > 0) {
+				return "employee-create-fund.jsp";
+			}
+
+			FundBean bean = fundDAO.read(form.getTicker());
+			if (bean != null) {
+				errors.add("The ticker you entered already exists.");
+				return "employee-create-fund.jsp";
+			}
+
+			FundBean fund = new FundBean();
+			fund.setName(form.getFundName());
+			fund.setSymbol(form.getTicker());
+
+			fundDAO.create(fund);
+			request.setAttribute("message", fund.getName() + " has been successfully created.");
+			return "success.jsp";
+
+		} catch (RollbackException e) {
+			errors.add(e.getMessage());
+			return "error.jsp";
+		} catch (Exception e) {
+			errors.add(e.getMessage());
+			return "error.jsp";
+		}
+	}
 }
