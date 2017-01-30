e4ac246b84d93c2b973a03647af14c943e38ca6f
Ajinkya Nimbalkar <animbalk@andrew.cmu.edu>
2017-01-30 16:47:05 -0500

    changed type date to text in transitionday.jsp

--- MutualFundWebApp/src/formbeans/TransitionDayForm.java
+++ MutualFundWebApp/src/formbeans/TransitionDayForm.java
@@ -18,7 +18,7 @@ public class TransitionDayForm {
 	private String transitionDayButton;
 
 	private boolean checkPrice = true;
-	private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+	private DateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy");
 	private Map<Integer, Double> map = new HashMap<Integer, Double>();
 
 	public TransitionDayForm(HttpServletRequest request) {
